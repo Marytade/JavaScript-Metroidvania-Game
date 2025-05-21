38ab8ef58547b3fe3cf5c5260539c419d4b5dbab
Mary Adebayo <mary.modupe04@gmail.com>
2025-05-21 11:55:45 -0500

    updated player mechanics.
    added kaboom functions to player mechanics as well as room1 functions.

---
+++ src/scenes/room1.js
@@ -0,0 +1,34 @@
+import { setBackgroundColor, setMapColliders } from "./roomUtils.js";
+
+export function room1(k, roomData) {
+    setBackgroundColor(k, "#8bb9b7");
+
+    k.camScale(4);
+    k.camPos(170, 100);
+    k.setGravity(1000);
+    
+    const roomLayers = roomData.layers;
+
+    const map = k.add([k.pos(0, 0), k.sprite("room1")]);
+    const colliders = [];
+    const positions = []
+    for (const layer of roomLayers) {
+        if (layer.name === "positions") {
+          positions.push(...layer.objects);
+          continue; 
+        } 
+
+        if (layer.name === "colliders") {
+            colliders.push(...layer.objects);
+            continue;
+        }
+    }
+   
+    setMapColliders(k, map, colliders);
+
+    const player = map.add(makelayer(k));
+
+    for (const position of positions) {
+        if (position.name=== "player")
+    }
+}
